b16b95106da47ca5aee67bdee9cf5352697d3931
仝曦文 <wb-txw886615@cainiao.com>
2021-11-19 12:53:11 +0800

    - kotlin库新增人民币纸币配色

---
+++ FoxDevFrame-Kotlin/src/main/java/com/binzeefox/foxdevframe_kotlin/utils/media/camera/CameraAction.kt
@@ -0,0 +1,12 @@
+package com.binzeefox.foxdevframe_kotlin.utils.media.camera
+
+/**
+ * 相机操作
+ *
+ * @author tong.xw
+ * 2021/06/21 10:08
+ */
+abstract class CameraAction {
+
+
+}
---
+++ FoxDevFrame-Kotlin/src/main/java/com/binzeefox/foxdevframe_kotlin/utils/media/camera/CameraUtil.kt
@@ -0,0 +1,10 @@
+package com.binzeefox.foxdevframe_kotlin.utils.media.camera
+
+/**
+ * 相机工具
+ *
+ * @author tong.xw
+ * 2021/06/21 10:07
+ */
+object CameraUtil {
+}
